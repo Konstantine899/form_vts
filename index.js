d49d47f035a11e779167d280f6b8b86839f82ec5
Атрощенко Константин Анатольевич <asu8@vts.vitebsk.energo.net>
2021-01-26 11:54:00 +0300

    connect to mysql

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const sequelize = require('./utils/database');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,4 +10,13 @@ app.use((req, res, next) => {
   res.sendFile('/index.html');
 });
 
-app.listen(PORT);
+async function start() {
+  try {
+    await sequelize.sync();
+    app.listen(PORT);
+  } catch (e) {
+    console.log('Ошибка подключения к БД', e);
+  }
+}
+
+start();
---
+++ utils/database.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+
+const DB_NAME = 'sampledb';
+const USER_NAME = 'root';
+const PASSWORD = '4343';
+
+const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+module.exports = sequelize;
